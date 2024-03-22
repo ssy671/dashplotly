50452b46b27b3876398f37b801a46afc2b33e945
ssy671 <ssy671@gmail.com>
2024-03-23 01:15:19 +0900

    test3

--- refreshtest.py
+++ refreshtest.py
@@ -24,6 +24,7 @@ app.layout = html.Div([
 # 다시 시도!!!!
 #반댜로 가즈베리에서 pc 로!!
 # 잘되는거같긴한데 다시 연습!!
+# 연습 add . - commit -m - push origin master
 # 콜백 함수: 그래프 업데이트
 @app.callback(
     Output('live-update-graph', 'figure'),
